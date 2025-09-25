78aecbf898e0832e0ecbf8f43fce983b109d1239
winchesterilad <winchester.ilad@evsu.edu.ph>
2025-09-25 22:29:00 +0800

    Delete it373-week2 directory

--- it373-week2/announcements/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Announcement(models.Model):
-    title = models.CharField(max_length=200)
-    summary = models.TextField()
-    content = models.TextField()
-
-    def __str__(self):
-        return self.title
